c8a0393c452bb6c0c672df4bf64391b9ce1f6e67
ericgaspar <junk.eg@free.fr>
2021-06-05 16:04:09 +0200

    Update config.local.js

--- conf/config.local.js
+++ conf/config.local.js
@@ -10,21 +10,21 @@ let config = window.mumbleWebConfig // eslint-disable-line no-unused-vars
 
 
 // Which fields to show on the Connect to Server dialog
-config.connectDialog.address = 'false'
-config.connectDialog.port = 'false'
-config.connectDialog.token = 'false'
-// config.connectDialog.username = 'true'
-// config.connectDialog.password = 'true'
-// config.connectDialog.channelName = 'false'
+config.connectDialog.address = false
+config.connectDialog.port = false
+config.connectDialog.token = false
+// config.connectDialog.username = true
+// config.connectDialog.password = true
+// config.connectDialog.channelName = false
 
 // Default values for user settings
 // You can see your current value by typing `localStorage.getItem('mumble.$setting')` in the web console.
 // config.settings.voiceMode = 'vad' // one of 'cont' (Continuous), 'ptt' (Push-to-Talk), 'vad' (Voice Activity Detection)
 // config.settings.pttKey = 'ctrl + shift'
 // config.settings.vadLevel = '0.3'
-// config.settings.toolbarVertical = 'false'
+// config.settings.toolbarVertical = false
 // config.settings.showAvatars = 'always' // one of 'always', 'own_channel', 'linked_channel', 'minimal_only', 'never'
-// config.settings.userCountInChannelName = 'false'
+// config.settings.userCountInChannelName = false
 // config.settings.audioBitrate = '40000' // bits per second
 // config.settings.samplesPerPacket = '960'
 
@@ -34,9 +34,9 @@ config.connectDialog.token = 'false'
 // config.defaults.port = '443'
 // config.defaults.token = ''
 // config.defaults.username = ''
-// config.defaults.password = '',
-// config.defaults.joinDialog = 'false' // replace whole dialog with single "Join Conference" button
-// config.defaults.matrix = 'false' // enable Matrix Widget support (mostly auto-detected; implies 'joinDialog')
+// config.defaults.password = ''
+// config.defaults.joinDialog = false // replace whole dialog with single "Join Conference" button
+// config.defaults.matrix = false // enable Matrix Widget support (mostly auto-detected; implies 'joinDialog')
 // config.defaults.avatarurl = '' // download and set the user's Mumble avatar to the image at this URL
 
 // General
